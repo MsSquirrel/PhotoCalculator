b4d62acdf22d9451d17c1488f65b739a8f42052b
Kristina <kristinasatara@gmail.com>
2016-03-30 02:24:05 +0200

    validation for 100 examples shows recognition 93%

--- PhotoCalculatorPONOVO/main.py
+++ PhotoCalculatorPONOVO/main.py
@@ -43,11 +43,28 @@ def rec_all(ann, naziv):
 def recognize_digit(ann, img):
 	test = matrix_to_vector(scale_to_range(img))	
 	test = test.reshape(1, test.shape[0]) # strange error when doing with both theano and numpy
-	print(test.shape)
 	result = ann.predict(test, batch_size=1)
 	brojevi = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 	ret_val =  display_result(result, brojevi)
 	return ret_val
+
+def validate_recognition(ann, path, file_name,validation_digit):
+	number_recognized = 0
+	
+	fajl = path+file_name
+	
+	with open(fajl) as f:
+		content = f.readlines()
+
+	for i in range(0,100):
+		naziv = path+content[i][:-1]
+		slicica = img_preproc_train(naziv)
+		val =  recognize_digit(ann, slicica)
+		if val[0] == validation_digit:
+			number_recognized+=1
+
+	print ("Recognized " + str(number_recognized))
+
 
 def main():
 
@@ -63,8 +80,10 @@ def main():
 	#print('Gotovo obucavanje')
 	#pickle.dump( ann1000_test, open("saveANN1000_test.p", "wb"))
 	ann1000_test = pickle.load( open( "saveANN1000_test.p", "rb" ))
-	#rec_dig(ann1000_test, 'digit_separated/digit_0/d0-0x00a69870.bmp')
-	rec_all(ann1000_test, 'images/img5.png')
+	#print ("Moja slika")
+	#rec_all(ann1000_test, 'images/img5.png')
+	print ("MNIST slicice")
+	validate_recognition(ann1000_test, 'digit_separated/digit_0/', 'test100.txt', '0')
 
 	#plt.imshow(avg[9], 'gray', interpolation='nearest')
 	#plt.waitforbuttonpress()
