d15bb050a1e939cd7c6910a73e5e0120d0f69e9c
Kristina <kristinasatara@gmail.com>
2016-03-31 00:39:31 +0200

    added () for recognition

--- PhotoCalculatorPONOVO/printRecognition.py
+++ PhotoCalculatorPONOVO/printRecognition.py
@@ -91,7 +91,7 @@ def create_ann_print():
     
     ann = Sequential()
     ann.add(Dense(1024, input_dim=784, activation='sigmoid'))
-    ann.add(Dense(14, activation='sigmoid'))
+    ann.add(Dense(16, activation='sigmoid'))
     return ann
 
 def train_ann_print(ann, X_train, y_train):
@@ -117,7 +117,7 @@ def display_result_print(outputs, alphabet):
 
 
 def print_recognition():
-	train_img = load_image('print_train/train.jpg')
+	train_img = load_image('print_train/train_basic.jpg')
 	train_bin = invert(image_bin(image_gray(train_img)))
 
 	n, kont = konture_print(train_img.copy(), train_bin)
@@ -127,27 +127,26 @@ def print_recognition():
 		konture.append(img_resize(k))
 		i+=1
 	
-	#plt.imshow(konture[11], 'gray')
+	#plt.imshow(konture[15], 'gray')
 	#plt.waitforbuttonpress()
 
-	#alphabet = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/']
-	#inputs = prepare_for_ann(konture)
-	#outputs = convert_output(alphabet)
+	alphabet = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/', '(', ')']
+	inputs = prepare_for_ann(konture)
+	outputs = convert_output(alphabet)
 	#ann_print = create_ann_print()
 	#ann_print = train_ann_print(ann_print, inputs, outputs)
-	#pickle.dump( ann_print, open("saveANN_stampano.p", "wb"))
+	#pickle.dump( ann_print, open("saveANN_stampanoZagrade.p", "wb"))
 	
-	ann_print = pickle.load( open( "saveANN_stampano.p", "rb" ) )
-	#recog_con(ann_print, kont[10])
-	#validate_print_recognition(ann_print)
-	print get_result('2+8*2')
+	ann_print = pickle.load( open( "saveANN_stampanoZagrade.p", "rb" ) )
+
+	validate_print_recognition(ann_print)
 
 
 def validate_print_recognition(ann):
 
 	print('Prepoznate cifre')
 	res=''
-	test_img = load_image('print_test/img2.jpg')
+	test_img = load_image('print_test/img3.jpg')
 	test_bin = invert(image_bin(image_gray(test_img)))
 	
 	n, kont = konture_print(test_img.copy(), test_bin)
@@ -156,16 +155,18 @@ def validate_print_recognition(ann):
 		konture.append(img_resize(k))
 
 	for k in konture:
-		res+=recog_con(ann, k)[0]
+		res+=recog_contour(ann, k)[0]
 
 	print res
+	print ('Result procesing '+exp_preproc(res))
+
 	#plt.imshow(konture[0], 'gray')
 	#plt.waitforbuttonpress()
 
 	#n, kont = konture_print(train_img.copy(), train_bin)
 
-def recog_con(ann_print, kont):
-	alphabet = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/']
+def recog_contour(ann_print, kont):
+	alphabet = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/', '(',')']
 	test_img = matrix_to_vector(scale_to_range(kont))
 	test_img = test_img.reshape(1, test_img.shape[0])
 	#print test_img.shape
@@ -174,6 +175,22 @@ def recog_con(ann_print, kont):
 	ret_val = display_result(result, alphabet)
 	return ret_val
 
+
+def exp_preproc(exp):
+
+	for i in range(len(exp)):
+		if exp[i]==')':
+			print ("nadjen ) na mestu"+ str(i))
+
+	return exp
+
+def is_digit(c):
+	if(c=='0' or c=='1' or c=='2' or c=='3' or c=='4'):
+		return True
+	if(c=='5' or c=='6' or c=='7' or c=='8' or c=='9'):
+		return True
+
+	return False
 
 def get_result(exp):
 
@@ -185,3 +202,4 @@ def get_result(exp):
     
 	ret_val = next(res.results).text
 	return ret_val
+
